9764ccca16869ed23919fe9e10e431b84fc3a377
Gulnaz <viteewato@mail.ru>
2024-07-28 18:18:57 +0300

    Start

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createElement } from 'react';
+import './index.css';
+import { App }  from './App';
+
+
+
+ReactDOM.render(
+	createElement(App),
+	 document.getElementById('root')
+)
+
+
+
+
